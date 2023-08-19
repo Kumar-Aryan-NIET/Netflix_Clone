2868f336b3c650745b105b20e81a3f209408a53c
Kumar Aryan <kumararyanaim@gmail.com>
2023-08-19 15:48:02 +0530

    Hero Section and Player functionality is Added

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,18 +1,20 @@
 import "./App.css";
 import React from "react";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Netflix from "./pages/Netflix";
+import Player from "./pages/Player";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path = "/login" element={<Login/>}/>
-      <Route exact path = "/signup" element={<SignUp/>}/>
-      <Route exact path = "/" element={<Netflix/>}/>
-    </Routes>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/player" element={<Player />} />
+        <Route exact path="/" element={<Netflix />} />
+      </Routes>
     </BrowserRouter>
   );
 }
